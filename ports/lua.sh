d5f7c0f43291b2b0ca512b65ffe28a7a8c357eec
Jeremy Soller <jackpot51@gmail.com>
2016-02-21 21:11:00 -0700

    Newlib update, fixes for many syscalls

--- ports/lua.sh
+++ ports/lua.sh
@@ -6,7 +6,6 @@ STABLE
 SRC=http://www.lua.org/ftp/lua-5.3.1.tar.gz
 DIR=lua-5.3.1
 
-export LINK_SCRIPT=${PWD}/../program.ld
 BUILD_ARGS="generic"
 INSTALL_ARGS="INSTALL_TOP=${PREFIX}"
 make_template $*
