f1f68cc1620f45976a222cf3ae703336aceada49
Jeremy Soller <jackpot51@gmail.com>
2016-02-22 11:43:36 -0700

    Add bochs hook for executables

--- newlib-redox/newlib/libc/sys/redox/crt0.c
+++ newlib-redox/newlib/libc/sys/redox/crt0.c
@@ -3,10 +3,12 @@ extern void exit(int code);
 extern int main(int argc, char ** argv, char ** envp);
 
 void _start(int args) {
+    asm volatile("xchgw %bx, %bx");
+
     int * params = &args-1;
     int argc = *params;
     char ** argv = (char **)(params + 1);
     environ = argv+argc+1;
-    
+
     exit(main(argc, argv, environ));
 }
