3ce4c3b11ad5f38ed75950ca3a826e60b18f6c8d
Jeremy Soller <jackpot51@gmail.com>
2016-07-31 12:35:04 -0600

    Add GCC port

---
+++ ports/gcc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source environ.sh
+
+BROKEN
+
+SRC=http://ftp.gnu.org/gnu/gcc/gcc-4.6.4/gcc-4.6.4.tar.bz2
+DIR=gcc-4.6.4
+
+CONFIGURE_ARGS="--host=i386-redox-gnu --prefix=$PREFIX"
+
+case $1 in
+    add)
+        fetch_template add
+        pushd "${BUILD}/${DIR}"
+        ./contrib/download_prerequisites
+        popd
+        configure_template configure
+        make_template build
+        make_template install
+        ;;
+    *)
+        configure_template $*
+        ;;
+esac
