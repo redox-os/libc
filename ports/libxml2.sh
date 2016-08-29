a1b2310ddce636eb9d2f7fb16be12328610ae9ac
Ian D. Scott <ian@iandouglasscott.com>
2016-08-28 22:15:30 -0700

    Make libxml2 build

--- ports/libxml2.sh
+++ ports/libxml2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source environ.sh
 
-BROKEN
+UNSTABLE
 
 SRC=ftp://xmlsoft.org/libxml2/libxml2-2.9.2.tar.gz
 DIR=libxml2-2.9.2
 
-CONFIGURE_ARGS="--host=i386-elf-redox --prefix=$PREFIX"
+CONFIGURE_ARGS="--host=i386-elf-redox --prefix=$PREFIX --without-ftp --without-http --without-python"
 configure_template $*
