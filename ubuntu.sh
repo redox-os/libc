4805aeed29219bdf4aca4da1bc55c1f0a8e938f1
Jeremy Soller <jackpot51@gmail.com>
2016-12-28 13:20:31 -0700

    Add more dependencies

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install autoconf2.64 automake1.11 gcc-multilib libtool texinfo gperf bison flex
+sudo apt-get install build-essential autoconf2.64 automake1.11 gcc-multilib libtool texinfo gperf bison flex python-minimal cmake
