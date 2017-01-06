98acf58c45ed0c52a2aa9bcf80d9ea2026aa2b49
Jeremy Soller <jackpot51@gmail.com>
2017-01-06 15:30:01 -0700

    Simplify netutils recipe

--- ports/netutils.sh
+++ ports/netutils.sh
@@ -6,6 +6,5 @@ UNSTABLE
 GIT=https://github.com/redox-os/netutils.git
 DIR=netutils
 
-CARGO_ARGS="--bin wget -- -Z print-link-args -C linker=x86_64-elf-redox-gcc"
 CARGO_BINS="dhcpd dns httpd irc nc ntp wget"
 cargo_template $*
