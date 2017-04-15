64292c182c3cf9ef16b7914f088dae505abefd34
Ian Douglas Scott <ian@iandouglasscott.com>
2017-04-14 20:36:31 -0700

    Do not unnecessarily call autotools

--- ports/binutils.sh
+++ ports/binutils.sh
@@ -7,19 +7,4 @@ GIT=https://github.com/redox-os/binutils-gdb.git
 DIR=binutils-gdb
 
 CONFIGURE_ARGS="--host=${HOST} --target=${HOST} --prefix=${PREFIX} --with-sysroot=${SYSROOT} --disable-gdb --disable-nls --disable-werror"
-
-case $1 in
-    add)
-        fetch_template add
-        mkdir -p "${BUILD}/${DIR}/${MAKE_DIR}"
-        pushd "${BUILD}/${DIR}/${MAKE_DIR}"
-            autoconf2.64 $AUTOCONF_ARGS
-        popd
-        configure_template configure
-        make_template build
-        make_template install
-        ;;
-    *)
-        configure_template $*
-        ;;
-esac
+configure_template $*
