bdccd730dd95611d4e23783db7c083b61cc81eba
Ian Douglas Scott <ian@iandouglasscott.com>
2017-04-14 22:38:15 -0700

    Make gcc compile, at least

--- ports/gcc.sh
+++ ports/gcc.sh
@@ -3,30 +3,32 @@ source environ.sh
 
 BROKEN
 
-SRC=http://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2
-DIR=gcc-4.9.4
+GIT=https://github.com/redox-os/gcc.git
+GIT_BRANCH=redox
+DIR=gcc
 
-CONFIGURE_ARGS="--host=${HOST} --target=${HOST} --enable-static --disable-shared --disable-dlopen --enable-languages=c"
+CONFIGURE_ARGS="--host=${HOST} --target=${HOST} --enable-static --disable-shared --disable-dlopen --disable-nls --enable-languages=c --without-headers"
+BUILD_ARGS="all-gcc all-target-libgcc"
 
 case $1 in
     add)
         fetch_template add
         pushd "${BUILD}/${DIR}"
         ./contrib/download_prerequisites
-        cp config.sub cloog/autoconf/config.sub
-        cp config.sub isl/config.sub
-        cp config.sub libjava/classpath/config.sub
-        cp config.sub gmp/configfsf.sub
-        cp config.sub mpc/config.sub
-        cp config.sub mpfr/config.sub
-        cp config.sub mpc/config.sub
+	cp config.sub gmp/config.sub
+	cp config.sub isl/config.sub
+	cp config.sub mpfr/config.sub
+	cp -f config.sub mpc/config.sub
         pushd libstdc++-v3
         autoconf2.64
         popd
         popd
         configure_template configure
         make_template build
-        make_template install
+        make -C "${BUILD}/${DIR}/${MAKE_DIR}" -j `nproc` install install-gcc install-target-libgcc
+        ;;
+    install)
+        make -C "${BUILD}/${DIR}/${MAKE_DIR}" -j `nproc` install install-gcc install-target-libgcc
         ;;
     *)
         configure_template $*
