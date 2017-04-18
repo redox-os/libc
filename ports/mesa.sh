5e349897c663018d4cfb8eb2428420f4926e43cc
Jeremy Soller <jackpot51@gmail.com>
2017-04-17 20:18:31 -0600

    WIP: Compile osmesa

--- ports/mesa.sh
+++ ports/mesa.sh
@@ -3,10 +3,10 @@ source environ.sh
 
 BROKEN
 
-GIT=https://github.com/redox-os/mesa.git
-GIT_BRANCH=redox
-DIR=mesa
+SRC=https://mesa.freedesktop.org/archive/12.0.1/mesa-12.0.1.tar.gz
+DIR=mesa-12.0.1
 
-CONFIGURE_ARGS="--host=${HOST} --enable-osmesa --disable-driglx-direct --disable-dri --disable-egl --disable-glx --disable-gbm --disable-omx"
-AUTOGEN_ARGS="${CONFIGURE_ARGS}"
-autogen_template $*
+export PTHREADSTUBS_CFLAGS="."
+export PTHREADSTUBS_LIBS="."
+CONFIGURE_ARGS="--host=${HOST} --disable-gles1 --disable-gles2 --disable-dri --disable-dri3 --disable-glx --disable-egl --disable-driglx-direct --enable-gallium-osmesa --with-gallium-drivers=swrast --disable-llvm-shared-libs"
+configure_template $*
