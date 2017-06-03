ec3398cf1616ddcc84a99fe955b0995c78547089
Ian Douglas Scott <ian@iandouglasscott.com>
2017-06-03 13:05:07 -0700

    Use / as prefix instead of /usr for binutils/gcc

--- ports/binutils.sh
+++ ports/binutils.sh
@@ -6,5 +6,26 @@ UNSTABLE
 GIT=https://github.com/redox-os/binutils-gdb.git
 DIR=binutils-gdb
 
-CONFIGURE_ARGS="--host=${HOST} --target=${HOST} --prefix=${PREFIX} --with-sysroot=${SYSROOT} --disable-gdb --disable-nls --disable-werror"
-configure_template $*
+CONFIGURE_ARGS="--host=${HOST} --target=${HOST} --prefix=/ --with-sysroot=${SYSROOT} --disable-gdb --disable-nls --disable-werror"
+
+function binutils_template {
+    case $1 in
+        install)
+            make -C "${BUILD}/${DIR}/${MAKE_DIR}" DESTDIR="${BUILD}/sysroot" -j `nproc` install
+            ;;
+        add)
+            fetch_template add
+            configure_template configure
+            make_template build
+            binutils_template install
+            ;;
+        uninstall)
+            make -C "${BUILD}/${DIR}/${MAKE_DIR}" DESTDIR="${BUILD}/sysroot" -j `nproc` uninstal
+            ;;
+        *)
+            configure_template $*
+            ;;
+    esac
+}
+
+binutils_template $*
