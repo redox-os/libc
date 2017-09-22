76380be85a18f5843110ef706b56c2eebc99fbd5
Jeremy Soller <jackpot51@gmail.com>
2017-09-21 20:26:59 -0600

    Update zlib

--- ports/zlib.sh
+++ ports/zlib.sh
@@ -3,8 +3,8 @@ source environ.sh
 
 STABLE
 
-SRC=http://zlib.net/zlib-1.2.8.tar.gz
-DIR=zlib-1.2.8
+SRC=http://zlib.net/zlib-1.2.11.tar.gz
+DIR=zlib-1.2.11
 
 CONFIGURE_ARGS="--static"
 configure_template $*
